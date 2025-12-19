f93783c39e645956d2643c6d08dcf747a2e0ddb9
SkipToMyLuLu <skip2mylulu@icloud.com>
2025-12-19 17:04:44 -0500

    Rechanged to christmas in html, added base code in js to start with

--- interactivity.js
+++ interactivity.js
@@ -1,4 +1,13 @@
-var test = document.getElementById("boat")
-var squoo = "Squanolk"
-test.innerHTML = "Test"
-console.log("Interactivity script loaded successfully.")
+
+
+
+function divUnformatted(ID, inputText) {
+    const divTarget = document.getElementById(ID)
+    console.info("Div aquired (Plain Text)")
+    divTarget.textContent = inputText;
+    console.log("Div updated (Plain text)")
+}
+
+// Christler
+divUnformatted("christmas", "Test Data")
+console.info('div plain txt fun. run, for chris')
