9b61cc8c082924f1b83207deb5a2c021e35b7472
SkipToMyLuLu <skip2mylulu@icloud.com>
2025-12-19 18:04:25 -0500

    Honestly i have no idea why it doesnt work, but it no worky

--- interactivity.js
+++ interactivity.js
@@ -3,8 +3,35 @@ function divUnformatted(ID) {
     console.info("Div aquired (Plain Text)")
     const month = divTarget.dataset.month
     const day = divTarget.dataset.day
+    var dated = divTarget.dataset.DateHyphen
+
+
+
+
     const time = new Date
-    
+
+    var yearTime = time.getFullYear()
+    var DateThe = dated += yearTime
+
+    // const theDate = time.getMonth(month) + time.getDay(day)
+    var theDate = DateThe
+
+    let timeRemaining = time.getTime() - theDate.getTime()
+    const millisecondsPerDay = 1000 * 60 * 60 * 24;
+
+    let remainingDayTime = timeRemaining / millisecondsPerDay
+    console.log(remainingDayTime)
+
+
+    // let monthRemain = time.getMonth() - month
+    // console.log("month remain + " + monthRemain)
+
+
+    // let dayRemain = time.getDay() - day
+    // console.log("dayremain " + dayRemain)
+
+
+    // divTarget.innerHTML 
 
 
 }
@@ -31,6 +58,7 @@ function callthings() {
     // console.info('div plain txt fun. run, for chris')
     divTime("CurrentDT")
     console.log("divtime")
+    divUnformatted("christmas")
 }
 
 callthings()
