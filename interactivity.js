bf57e41bd1a09f91539e4145a836b088a726afe0
SkipToMyLuLu <skip2mylulu@icloud.com>
2025-12-19 17:43:37 -0500

    stupid amount of js

--- interactivity.js
+++ interactivity.js
@@ -1,13 +1,36 @@
-
-
-
-function divUnformatted(ID, inputText) {
+function divUnformatted(ID) {
     const divTarget = document.getElementById(ID)
     console.info("Div aquired (Plain Text)")
-    divTarget.textContent = inputText;
-    console.log("Div updated (Plain text)")
+    const month = divTarget.dataset.month
+    const day = divTarget.dataset.day
+    const time = new Date
+    
+
+
 }
 
+
+function divTime(ID) {
+    const divTarget = document.getElementById(ID)
+    console.info("Div aquired (Plain Text)")
+    var time = new Date
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+    // const options = {}
+    const displayDate = time.toLocaleDateString(undefined, options);
+
+    divTarget.innerHTML = displayDate
+}
+
+
 // Christler
-divUnformatted("christmas", "Test Data")
-console.info('div plain txt fun. run, for chris')
+// divUnformatted("christmas", "Test Data")
+// console.info('div plain txt fun. run, for chris')
+
+function callthings() {
+    // divUnformatted("christmas", "Test Data")
+    // console.info('div plain txt fun. run, for chris')
+    divTime("CurrentDT")
+    console.log("divtime")
+}
+
+callthings()
